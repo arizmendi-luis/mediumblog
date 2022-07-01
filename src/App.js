600429ef0a183cf0a11db5b11efb04398cece102
Luis A <46586879+arizmendi-luis@users.noreply.github.com>
2022-07-01 10:16:01 -0700

    joeligma

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React hello World my name is JOeLigma
         </a>
       </header>
     </div>
