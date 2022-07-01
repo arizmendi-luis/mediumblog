e89b8be9f5f8c47e552b7e7b033a45d6392ef7c2
Luis A <46586879+arizmendi-luis@users.noreply.github.com>
2022-07-01 10:52:23 -0700

    tutorialadd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn ReactJOELIGMAWENDYS
+          Learn React Tutorial
         </a>
       </header>
     </div>
